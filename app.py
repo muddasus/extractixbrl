bbee5355767e56a9f528abdf08c6163c73ade863
muddasus <38861086+muddasus@users.noreply.github.com>
2025-03-19 22:17:17 -0400

    return annual return

--- app.py
+++ app.py
@@ -74,6 +74,8 @@ def extract_ixbrl_data(filing_url):
 
         expensepct = pd.json_normalize(xbrl_json.get("ExpenseRatioPct", {}))
         expenseamt = pd.json_normalize(xbrl_json.get("ExpensesPaidAmt", {}))
+        AvgAnnualReturnPct = pd.json_normalize(xbrl_json.get("AvgAnnlRtrPct", {}))
+
 
         if "segment.value" in expenseamt.columns:
             expenseamt["classid"] = expenseamt["segment.value"].apply(extract_classid)
@@ -81,6 +83,7 @@ def extract_ixbrl_data(filing_url):
         combined_expenses = pd.merge(expensepct, expenseamt, left_index=True, right_index=True)
         combined_expenses = combined_expenses.rename(columns={"value_x": "expense_pct", "value_y": "expense_amt"})
         combined_expenses["filingURL"] = filing_url
+        combined_expenses["AvgAnnualReturnPct"] = AvgAnnualReturnPct
 
         return combined_expenses
 
